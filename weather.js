20ed83d86252f2395a73e9ac5946154ab750a781
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-19 19:02:43 -0400

    changed http to https

--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ class Weather {
 
     async getWeather() {
 
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state},${this.country}&units=metric&appid=${this.apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state},${this.country}&units=metric&appid=${this.apiKey}`)
 
         const responseData = await response.json();
         
